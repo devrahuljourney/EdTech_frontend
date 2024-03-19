34e300537aa756e7000cd8d81da6f1613c89801c
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-03-19 15:57:09 +0530

    backend linked

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+//const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://edtech-backend-1-n1v7.onrender.com/api/v1"
+
 
 // AUTH ENDPOINTS
 export const endpoints = {
